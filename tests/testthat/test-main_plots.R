8e0f3775e76d0ebcd189eb0412a56c5f4633d2fe
Mark C. Wheldon <wheldon@un.org>
2026-04-03 15:01:47 -0400

    Fix small mistake in test of main plots.

--- tests/testthat/test-main_plots.R
+++ tests/testthat/test-main_plots.R
@@ -2,7 +2,7 @@
 ### * Set Up
 
 cl <- parallel::makeCluster(parallelly::availableCores(omit = 2))
-registerDoParallel(cl)
+doParallel::registerDoParallel(cl)
 
 ###-----------------------------------------------------------------------------
 ### ** Data Frame Methods
@@ -15,26 +15,29 @@ test_that("'plot_tfr_surfs.data.frame()' works, no x_alt.", {
     param_df <- make_plot_tfr_surfs_param_df()
     k <- "716"
 
-    results <- foreach(param_i = seq_len(nrow(param_df))) %dopar% {
-        library(tfrSURFs)
+    results <-
+        expect_error(
+            foreach(param_i = seq_len(nrow(param_df))) %dopar% {
+                library(tfrSURFs)
 
-        ## for (param_i in seq_len(nrow(param_df))) {
-        ## cat("\n\ni = ", param_i, "\n\n")
+                ## for (param_i in seq_len(nrow(param_df))) {
+                ## cat("\n\ni = ", param_i, "\n\n")
 
-        pars <- param_df[param_i, , drop = FALSE]
-        suppressMessages(suppressWarnings(
-            plot_tfr_surfs(test_data_tfrSURFs_list[[k]],
-                           yvar = pars[["yvar"]],
-                           add_prob_ref_lines = pars[["add_prob_ref_lines"]],
-                           add_range_regions = pars[["add_range_regions"]],
-                           add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
-                           add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
-                           maximal_legend = pars[["maximal_legend"]],
-                           add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
-                           add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
-                           datestamp = pars[["datestamp"]]
-                           )))
-    }
+                pars <- param_df[param_i, , drop = FALSE]
+                suppressMessages(suppressWarnings(
+                    plot_tfr_surfs(test_data_tfrSURFs_list[[k]],
+                                   yvar = pars[["yvar"]],
+                                   add_prob_ref_lines = pars[["add_prob_ref_lines"]],
+                                   add_range_regions = pars[["add_range_regions"]],
+                                   add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
+                                   add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
+                                   maximal_legend = pars[["maximal_legend"]],
+                                   add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
+                                   add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
+                                   datestamp = pars[["datestamp"]]
+                                   )))
+            },
+            NA)
 })
 
 
@@ -43,24 +46,27 @@ test_that("'plot_tfr_surfs.data.frame()' works, with x_alt.", {
     param_df <- make_plot_tfr_surfs_param_df()
     k <- "716"
 
-    results <- foreach(param_i = seq_len(nrow(param_df))) %dopar% {
-        library(tfrSURFs)
-        pars <- param_df[param_i, , drop = FALSE]
+    results <-
+        expect_error(
+            foreach(param_i = seq_len(nrow(param_df))) %dopar% {
+                library(tfrSURFs)
+                pars <- param_df[param_i, , drop = FALSE]
 
-    plot_tfr_surfs(test_data_tfrSURFs_list[[k]],
-                   yvar = pars[["yvar"]],
-                   x_alt = test_data_tfrSURFs_median_list[[k]],
-                   x_alt_label = "Median only",
-                   add_prob_ref_lines = pars[["add_prob_ref_lines"]],
-                   add_range_regions = pars[["add_range_regions"]],
-                   add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
-                   add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
-                   maximal_legend = pars[["maximal_legend"]],
-                   add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
-                   add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
-                   datestamp = pars[["datestamp"]]
-                   )
-        }
+                plot_tfr_surfs(test_data_tfrSURFs_list[[k]],
+                               yvar = pars[["yvar"]],
+                               x_alt = test_data_tfrSURFs_median_list[[k]],
+                               x_alt_label = "Median only",
+                               add_prob_ref_lines = pars[["add_prob_ref_lines"]],
+                               add_range_regions = pars[["add_range_regions"]],
+                               add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
+                               add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
+                               maximal_legend = pars[["maximal_legend"]],
+                               add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
+                               add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
+                               datestamp = pars[["datestamp"]]
+                               )
+            },
+            NA)
 })
 
 ###-----------------------------------------------------------------------------
@@ -71,23 +77,26 @@ test_that("'plot_surfs_probs.data.frame()' works, no x_alt.", {
     param_df <- make_plot_surfs_probs_param_df()
     k <- "716"
 
-    results <- foreach(param_i = seq_len(nrow(param_df))) %dopar% {
-        library(tfrSURFs)
+    results <-
+        expect_error(
+            foreach(param_i = seq_len(nrow(param_df))) %dopar% {
+                library(tfrSURFs)
 
-        pars <- param_df[param_i, , drop = FALSE]
-        suppressMessages(suppressWarnings(
-            plot_surfs_probs(test_data_tfrSURFs_list[[k]],
-                           yvar = pars[["yvar"]],
-                           add_prob_ref_lines = pars[["add_prob_ref_lines"]],
-                           add_range_regions = pars[["add_range_regions"]],
-                           add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
-                           add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
-                           maximal_legend = pars[["maximal_legend"]],
-                           add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
-                           add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
-                           datestamp = pars[["datestamp"]]
-                           )))
-    }
+                pars <- param_df[param_i, , drop = FALSE]
+                suppressMessages(suppressWarnings(
+                    plot_surfs_probs(test_data_tfrSURFs_list[[k]],
+                                     yvar = pars[["yvar"]],
+                                     add_prob_ref_lines = pars[["add_prob_ref_lines"]],
+                                     add_range_regions = pars[["add_range_regions"]],
+                                     add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
+                                     add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
+                                     maximal_legend = pars[["maximal_legend"]],
+                                     add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
+                                     add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
+                                     datestamp = pars[["datestamp"]]
+                                     )))
+            },
+            NA)
 })
 
 
@@ -96,24 +105,27 @@ test_that("'plot_surfs_probs.data.frame()' works, with x_alt.", {
     param_df <- make_plot_surfs_probs_param_df()
     k <- "716"
 
-    results <- foreach(param_i = seq_len(nrow(param_df))) %dopar% {
-        library(tfrSURFs)
-        pars <- param_df[param_i, , drop = FALSE]
+    results <-
+        expect_error(
+            foreach(param_i = seq_len(nrow(param_df))) %dopar% {
+                library(tfrSURFs)
+                pars <- param_df[param_i, , drop = FALSE]
 
-    plot_surfs_probs(test_data_tfrSURFs_list[[k]],
-                   yvar = pars[["yvar"]],
-                   x_alt = test_data_tfrSURFs_median_list[[k]],
-                   x_alt_label = "Median only",
-                   add_prob_ref_lines = pars[["add_prob_ref_lines"]],
-                   add_range_regions = pars[["add_range_regions"]],
-                   add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
-                   add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
-                   maximal_legend = pars[["maximal_legend"]],
-                   add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
-                   add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
-                   datestamp = pars[["datestamp"]]
-                   )
-        }
+                plot_surfs_probs(test_data_tfrSURFs_list[[k]],
+                                 yvar = pars[["yvar"]],
+                                 x_alt = test_data_tfrSURFs_median_list[[k]],
+                                 x_alt_label = "Median only",
+                                 add_prob_ref_lines = pars[["add_prob_ref_lines"]],
+                                 add_range_regions = pars[["add_range_regions"]],
+                                 add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
+                                 add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
+                                 maximal_legend = pars[["maximal_legend"]],
+                                 add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
+                                 add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
+                                 datestamp = pars[["datestamp"]]
+                                 )
+            },
+            NA)
 })
 
 ###-----------------------------------------------------------------------------
@@ -126,23 +138,26 @@ test_that("'plot_tfr_surfs.list()' works, no x_alt.", {
 
     param_df <- make_plot_tfr_surfs_param_df()
 
-    results <- foreach(param_i = seq_len(nrow(param_df))) %dopar% {
-        library(tfrSURFs)
+    results <-
+        expect_error(
+            foreach(param_i = seq_len(nrow(param_df))) %dopar% {
+                library(tfrSURFs)
 
-        pars <- param_df[param_i, , drop = FALSE]
-        suppressMessages(suppressWarnings(
-            plot_tfr_surfs(test_data_tfrSURFs_list,
-                           yvar = pars[["yvar"]],
-                           add_prob_ref_lines = pars[["add_prob_ref_lines"]],
-                           add_range_regions = pars[["add_range_regions"]],
-                           add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
-                           add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
-                           maximal_legend = pars[["maximal_legend"]],
-                           add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
-                           add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
-                           datestamp = pars[["datestamp"]]
-                           )))
-    }
+                pars <- param_df[param_i, , drop = FALSE]
+                suppressMessages(suppressWarnings(
+                    plot_tfr_surfs(test_data_tfrSURFs_list,
+                                   yvar = pars[["yvar"]],
+                                   add_prob_ref_lines = pars[["add_prob_ref_lines"]],
+                                   add_range_regions = pars[["add_range_regions"]],
+                                   add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
+                                   add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
+                                   maximal_legend = pars[["maximal_legend"]],
+                                   add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
+                                   add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
+                                   datestamp = pars[["datestamp"]]
+                                   )))
+            },
+            NA)
 })
 
 
@@ -151,24 +166,27 @@ test_that("'plot_tfr_surfs.list()' works, with x_alt.", {
     param_df <- make_plot_tfr_surfs_param_df()
     k <- "716"
 
-    results <- foreach(param_i = seq_len(nrow(param_df))) %dopar% {
-        library(tfrSURFs)
-        pars <- param_df[param_i, , drop = FALSE]
+    results <-
+        expect_error(
+            foreach(param_i = seq_len(nrow(param_df))) %dopar% {
+                library(tfrSURFs)
+                pars <- param_df[param_i, , drop = FALSE]
 
-    plot_tfr_surfs(test_data_tfrSURFs_list,
-                   yvar = pars[["yvar"]],
-                   x_alt = test_data_tfrSURFs_median_list,
-                   x_alt_label = "Median only",
-                   add_prob_ref_lines = pars[["add_prob_ref_lines"]],
-                   add_range_regions = pars[["add_range_regions"]],
-                   add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
-                   add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
-                   maximal_legend = pars[["maximal_legend"]],
-                   add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
-                   add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
-                   datestamp = pars[["datestamp"]]
-                   )
-        }
+                plot_tfr_surfs(test_data_tfrSURFs_list,
+                               yvar = pars[["yvar"]],
+                               x_alt = test_data_tfrSURFs_median_list,
+                               x_alt_label = "Median only",
+                               add_prob_ref_lines = pars[["add_prob_ref_lines"]],
+                               add_range_regions = pars[["add_range_regions"]],
+                               add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
+                               add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
+                               maximal_legend = pars[["maximal_legend"]],
+                               add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
+                               add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
+                               datestamp = pars[["datestamp"]]
+                               )
+            },
+            NA)
 })
 
 ###-----------------------------------------------------------------------------
@@ -178,23 +196,26 @@ test_that("'plot_surfs_probs.list()' works, no x_alt.", {
 
     param_df <- make_plot_surfs_probs_param_df()
 
-    results <- foreach(param_i = seq_len(nrow(param_df))) %dopar% {
-        library(tfrSURFs)
+    results <-
+        expect_error(
+            foreach(param_i = seq_len(nrow(param_df))) %dopar% {
+                library(tfrSURFs)
 
-        pars <- param_df[param_i, , drop = FALSE]
-        suppressMessages(suppressWarnings(
-            plot_surfs_probs(test_data_tfrSURFs_list,
-                           yvar = pars[["yvar"]],
-                           add_prob_ref_lines = pars[["add_prob_ref_lines"]],
-                           add_range_regions = pars[["add_range_regions"]],
-                           add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
-                           add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
-                           maximal_legend = pars[["maximal_legend"]],
-                           add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
-                           add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
-                           datestamp = pars[["datestamp"]]
-                           )))
-    }
+                pars <- param_df[param_i, , drop = FALSE]
+                suppressMessages(suppressWarnings(
+                    plot_surfs_probs(test_data_tfrSURFs_list,
+                                     yvar = pars[["yvar"]],
+                                     add_prob_ref_lines = pars[["add_prob_ref_lines"]],
+                                     add_range_regions = pars[["add_range_regions"]],
+                                     add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
+                                     add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
+                                     maximal_legend = pars[["maximal_legend"]],
+                                     add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
+                                     add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
+                                     datestamp = pars[["datestamp"]]
+                                     )))
+            },
+            NA)
 })
 
 
@@ -202,24 +223,27 @@ test_that("'plot_surfs_probs.list()' works, with x_alt.", {
 
     param_df <- make_plot_surfs_probs_param_df()
 
-    results <- foreach(param_i = seq_len(nrow(param_df))) %dopar% {
-        library(tfrSURFs)
-        pars <- param_df[param_i, , drop = FALSE]
+    results <-
+        expect_error(
+            foreach(param_i = seq_len(nrow(param_df))) %dopar% {
+                library(tfrSURFs)
+                pars <- param_df[param_i, , drop = FALSE]
 
-    plot_surfs_probs(test_data_tfrSURFs_list,
-                   yvar = pars[["yvar"]],
-                   x_alt = test_data_tfrSURFs_median_list,
-                   x_alt_label = "Median only",
-                   add_prob_ref_lines = pars[["add_prob_ref_lines"]],
-                   add_range_regions = pars[["add_range_regions"]],
-                   add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
-                   add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
-                   maximal_legend = pars[["maximal_legend"]],
-                   add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
-                   add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
-                   datestamp = pars[["datestamp"]]
-                   )
-        }
+                plot_surfs_probs(test_data_tfrSURFs_list,
+                                 yvar = pars[["yvar"]],
+                                 x_alt = test_data_tfrSURFs_median_list,
+                                 x_alt_label = "Median only",
+                                 add_prob_ref_lines = pars[["add_prob_ref_lines"]],
+                                 add_range_regions = pars[["add_range_regions"]],
+                                 add_est_proj_ref_line = pars[["add_est_proj_ref_line"]],
+                                 add_Schoumaker_stalls = pars[["add_Schoumaker_stalls"]],
+                                 maximal_legend = pars[["maximal_legend"]],
+                                 add_prob_TFR_surfs = pars[["add_prob_TFR_surfs"]],
+                                 add_prob_TFR_surf_projections = pars[["add_prob_TFR_surf_projections"]],
+                                 datestamp = pars[["datestamp"]]
+                                 )
+            },
+            NA)
 })
 
 ###-----------------------------------------------------------------------------
